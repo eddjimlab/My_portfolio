7c32e979ae69ddb78085ec47bfef835fb8fe298a
Eduard <eddjimlab@gmail.com>
2019-11-01 18:12:59 +0300

    fix some mistakes

--- src/boot/main.js
+++ src/boot/main.js
@@ -1,6 +1,3 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 
-Vue.use(VueResource)
-Vue.http.options.root = 'http://localhost:3000/'
 Vue.prototype.$urlReq = 'http://localhost:3000/items/'
